3a340776abd93a01b78d23d528c044ee51ae2e77
kelsieszost <owner@kelsieszost.design>
2022-11-20 19:54:51 -0500

    thoughtSchema

--- models/thought.js
+++ models/thought.js
@@ -33,3 +33,35 @@ const reactionSchema = new Schema (
         },
     }
 );
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) =>
+                moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+
+        id: false,
+    }
+);
